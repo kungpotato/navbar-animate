ce527bc2b95e0d109e6f778cec4f971a9af0e69e
kungpotato <kittisak.py@gmail.com>
2017-07-08 22:07:16 +0700

    edit animation

--- script/script.js
+++ script/script.js
@@ -12,13 +12,11 @@ $(document).ready(function(){
 $(document).ready(function() {
     $(window).scroll(function() { // check if scroll event happened
         if ($(document).scrollTop() > 0) {
-            $(".navbar-default").animate({ backgroundColor: "white" }, '300');  /** need jquery-ui */
+            $(".navbar-default").animate({ backgroundColor: "white", padding: "10px" }, '100');  /** need jquery-ui */
             $(".navbar-default").css("box-shadow", "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)");
-            $(".navbar-default").css("padding-top","10px")
         } else {
-            $(".navbar-default").stop(true).animate({ backgroundColor: "transparent" }, '300');
+            $(".navbar-default").stop(true).animate({ backgroundColor: "transparent", padding: "30px" }, '100');
             $(".navbar-default").css("box-shadow", "none");
-            $(".navbar-default").css("padding-top","50px")
         }
     });
 });
